56caf533ad445abbc133f01645e3c4a4c726ee98
shiqingqing <750633982@qq.com>
2019-02-25 23:54:47 +0800

    111

--- app/src/main/java/com/wb/weibao/base/ThisApplication.java
+++ app/src/main/java/com/wb/weibao/base/ThisApplication.java
@@ -2,11 +2,13 @@ package com.wb.weibao.base;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * Created by LiMing on 2016/11/12.
  */
-public class ThisApplication extends Application {
+public class ThisApplication extends MultiDexApplication {
     public static Context applicationContext;
     private static ThisApplication instance;
     @Override
@@ -20,5 +22,10 @@ public class ThisApplication extends Application {
         return instance;
     }
 
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this); // 初始化
+    }
 
 }
--- app/src/main/java/com/wb/weibao/common/Link.java
+++ app/src/main/java/com/wb/weibao/common/Link.java
@@ -6,7 +6,8 @@ package com.wb.weibao.common;
 
 public class Link {
 //    public static final String SEREVE = "http://dev.zephcard.com:3000/earlywarn/";
-    public static final String SEREVE = "http://admin.zjcyxf.com/earlywarn/";
+//    public static final String SEREVE = "http://admin.zjcyxf.com/earlywarn/";
+    public static final String SEREVE = "http://47.110.184.79:8300/earlywarn/";
     public static final String PHOTOSEREVE = "http://101.236.25.68:81/security-monitor/app";
 
 

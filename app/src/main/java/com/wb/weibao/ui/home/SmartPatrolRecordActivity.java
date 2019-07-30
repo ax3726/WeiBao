c0d6d0d95d2bde18e4a3023d94866a796cbf4747
liming <15970060419lma>
2019-07-30 10:20:08 +0800

    tjiao11111

--- app/src/main/java/com/wb/weibao/ui/home/SmartPatrolRecordActivity.java
+++ app/src/main/java/com/wb/weibao/ui/home/SmartPatrolRecordActivity.java
@@ -1,28 +1,45 @@
 package com.wb.weibao.ui.home;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.TypedValue;
 import android.view.View;
+import android.widget.TextView;
 
+import com.lling.photopicker.PhotoPickerActivity;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 import com.scwang.smartrefresh.layout.listener.OnRefreshLoadmoreListener;
 import com.wb.weibao.R;
 import com.wb.weibao.adapters.recyclerview.CommonAdapter;
 import com.wb.weibao.adapters.recyclerview.base.ViewHolder;
 import com.wb.weibao.base.BaseActivity;
+import com.wb.weibao.base.BaseNetListener;
 import com.wb.weibao.base.BasePresenter;
+import com.wb.weibao.common.Api;
 import com.wb.weibao.databinding.ActivitySmartPatrolRecordBinding;
 import com.wb.weibao.databinding.ItemSmartPatrolRecordBinding;
+import com.wb.weibao.model.BaseBean;
+import com.wb.weibao.utils.DemoUtils;
 import com.wb.weibao.view.PopupWindow.ConfirmPopupwindow;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+
 public class SmartPatrolRecordActivity extends BaseActivity<BasePresenter, ActivitySmartPatrolRecordBinding> {
 
-    private List<String> mDataList = new ArrayList<>();
+    private List<String>          mDataList           = new ArrayList<>();
     private CommonAdapter<String> mAdapter;
-    private int mPage = 1;
-    private int mPageSize = 10;
+    private int                   mPage               = 1;
+    private int                   mPageSize           = 10;
+    private ConfirmPopupwindow    mConfirmPopupwindow = null;
+
     @Override
     protected boolean isTitleBar() {
         return true;
@@ -53,6 +70,28 @@ public class SmartPatrolRecordActivity extends BaseActivity<BasePresenter, Activ
     }
 
     @Override
+    protected void initView(Bundle savedInstanceState) {
+        super.initView(savedInstanceState);
+        TextView txt_search = (TextView) mBinding.searchview.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+        //设置字体大小为14sp
+        txt_search.setTextSize(TypedValue.COMPLEX_UNIT_SP, 14);//14sp
+        //设置字体颜色
+        txt_search.setTextColor(getResources().getColor(R.color.color333333));
+        //设置提示文字颜色
+        txt_search.setHintTextColor(getResources().getColor(R.color.color999999));
+        //去掉searchview下划线
+        View view = mBinding.searchview.findViewById(android.support.v7.appcompat.R.id.search_plate);
+        view.setBackgroundColor(Color.TRANSPARENT);
+
+    }
+
+    @Override
+    protected void initEvent() {
+        super.initEvent();
+
+    }
+
+    @Override
     protected void initData() {
         super.initData();
         mDataList.add("");
@@ -69,8 +108,14 @@ public class SmartPatrolRecordActivity extends BaseActivity<BasePresenter, Activ
                 binding.tvOk.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        ConfirmPopupwindow confirmPopupwindow=new ConfirmPopupwindow(aty);
-                        confirmPopupwindow.showPopupWindow();
+                        mConfirmPopupwindow = new ConfirmPopupwindow(aty);
+                        mConfirmPopupwindow.setConfirmListener(new ConfirmPopupwindow.ConfirmListener() {
+                            @Override
+                            public void onOk(ArrayList<String> imageUUid, String txt) {//返回结果
+
+                            }
+                        });
+                        mConfirmPopupwindow.showPopupWindow();
                     }
                 });
             }
@@ -107,4 +152,50 @@ public class SmartPatrolRecordActivity extends BaseActivity<BasePresenter, Activ
         mBinding.srlBody.finishLoadmore();
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == PhotoPickerActivity.RESULT_OK && requestCode == ConfirmPopupwindow.RequestCode) {
+            ArrayList<String> result = data.getStringArrayListExtra(PhotoPickerActivity.KEY_RESULT);
+            if (mConfirmPopupwindow != null) {
+                if (result != null && result.size() > 0) {
+                    loadImg(result.get(0));
+                    mConfirmPopupwindow.setData(result.get(0));
+                    mAdapter.notifyDataSetChanged();
+
+                }
+
+            }
+        }
+    }
+
+    private void loadImg(String str) {
+//        if (!TextUtils.isEmpty(str)) {
+        File   file  = new File(str);
+        byte[] bytes = DemoUtils.getimageByte(str);
+        // MultipartBody.Part  和后端约定好Key，这里的partName是用image
+
+        MultipartBody.Part body = MultipartBody.Part.createFormData("file", file.getName(), RequestBody.create(MediaType.parse("image/png"), bytes));
+
+
+        Api.getApi().upLoad(body)
+                .compose(callbackOnIOToMainThread()).subscribe(new BaseNetListener<BaseBean>(this, true) {
+            @Override
+            public void onSuccess(BaseBean baseBean) {
+                if (mConfirmPopupwindow != null) {
+                    mConfirmPopupwindow.addData(baseBean.getData().toString());
+                }
+
+            }
+
+            @Override
+            public void onFail(String errMsg) {
+//                    mBinding.affirm.setEnabled(true);
+            }
+        });
+//        } else {
+//            addRecord();
+//        }
+
+    }
 }
--- app/src/main/java/com/wb/weibao/view/PopupWindow/ConfirmPopupwindow.java
+++ app/src/main/java/com/wb/weibao/view/PopupWindow/ConfirmPopupwindow.java
@@ -18,7 +18,6 @@ import com.lling.photopicker.PhotoPickerActivity;
 import com.wb.weibao.R;
 import com.wb.weibao.adapters.abslistview.CommonAdapter;
 import com.wb.weibao.databinding.PopuwindowsOkLayoutBinding;
-import com.wb.weibao.model.main.ChooseTypeModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,13 +28,13 @@ import java.util.List;
  */
 
 public class ConfirmPopupwindow extends PopupWindow {
-    private Activity aty;
-    private PopuwindowsOkLayoutBinding mBinding;
-    private ArrayList<String> mImageUUid = new ArrayList<>();
-    private ChooseTypeListener mChooseTypeListener;
-    public final int RequestCode = 1001;
-    private List<String> mImgs = new ArrayList<>();
-    private CommonAdapter<String> mAdapter;
+    private             Activity                   aty;
+    private             PopuwindowsOkLayoutBinding mBinding;
+    private             ArrayList<String>          mImageUUid  = new ArrayList<>();
+    private             ConfirmListener            mConfirmListener;
+    public static final int                        RequestCode = 1001;
+    private             List<String>               mImgs       = new ArrayList<>();
+    private             CommonAdapter<String>      mAdapter;
 
     public ConfirmPopupwindow(Activity activity) {
         aty = activity;
@@ -69,15 +68,51 @@ public class ConfirmPopupwindow extends PopupWindow {
     }
 
     private void initView() {
+        mBinding.tvCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        mBinding.tvOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String content = mBinding.etContent.getText().toString().trim();
+                if (mConfirmListener != null) {
+                    mConfirmListener.onOk(mImageUUid, content);
+                }
+                dismiss();
+
+
+            }
+        });
         initAdapter();
         this.update();
+
     }
+
+    public void addData(String str) {
+        mImageUUid.add(str);
+    }
+
+    public void setData(String img) {
+
+        mImgs.set(mImgs.size() - 1, img);
+        if (mImgs.size() < 4) {
+            mImgs.add("");
+        }
+
+        mAdapter.notifyDataSetChanged();
+
+
+    }
+
     private void initAdapter() {
         mImgs.add("");
         mAdapter = new com.wb.weibao.adapters.abslistview.CommonAdapter<String>(aty, R.layout.item_add_point_img_layout, mImgs) {
             @Override
             protected void convert(com.wb.weibao.adapters.abslistview.ViewHolder viewHolder, String item, int position) {
-                ImageView img = viewHolder.getView(R.id.img);
+                ImageView img     = viewHolder.getView(R.id.img);
                 ImageView img_del = viewHolder.getView(R.id.img_del);
 
                 if (TextUtils.isEmpty(item)) {
@@ -106,7 +141,7 @@ public class ConfirmPopupwindow extends PopupWindow {
                             mImgs.add("");
                         }
                         mImgs.remove(position);
-                      //  mImageUUid.remove(position);
+                        mImageUUid.remove(position);
                         notifyDataSetChanged();
                     }
                 });
@@ -121,7 +156,7 @@ public class ConfirmPopupwindow extends PopupWindow {
     public void showPopupWindow() {
         if (!this.isShowing()) {
             // 以下拉方式显示popupwindow
-            this.showAtLocation(getRootView(), Gravity.BOTTOM,0, 0);
+            this.showAtLocation(getRootView(), Gravity.BOTTOM, 0, 0);
         } else {
             // this.dismiss();
         }
@@ -143,12 +178,12 @@ public class ConfirmPopupwindow extends PopupWindow {
         return ((ViewGroup) aty.findViewById(android.R.id.content)).getChildAt(0);
     }
 
-    public void setChooseTypeListener(ChooseTypeListener mChooseTypeListener) {
-        this.mChooseTypeListener = mChooseTypeListener;
+    public void setConfirmListener(ConfirmListener mConfirmListener) {
+        this.mConfirmListener = mConfirmListener;
     }
 
-    public interface ChooseTypeListener {
-        void onItem(ChooseTypeModel item);//选中内容
+    public interface ConfirmListener {
+        void onOk(ArrayList<String> imageUUid, String txt);//
     }
 
 }
